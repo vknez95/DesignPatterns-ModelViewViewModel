4cec4f6a0d950a6f0823f41f8fbe2ced450c9e79
vknez95 <veljko.knezevic@gmail.com>
2017-09-24 13:10:00 -0400

    init

---
+++ SimpleViewModel/Models/Person.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SimpleMvvm
+{
+    public class Person : INotifyPropertyChanged, IDataErrorInfo
+    {
+        #region Properties
+
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+
+        private DateTime _lastUpdated = DateTime.Now;
+        public DateTime LastUpdated
+        {
+            get { return _lastUpdated; }
+            set
+            {
+                _lastUpdated = value;
+                OnPropertyChanged("LastUpdated");
+            }
+        }
+
+        #endregion //Properties
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyname)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyname));
+        }
+
+        #endregion //INotifyPropertyChanged
+
+        #region IDataErrorInfo
+
+        public string Error
+        {
+            get { return null; }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string error = null;
+
+                switch (columnName)
+                {
+                    case "FirstName":
+                        if (string.IsNullOrEmpty(_firstName))
+                        {
+                            error = "First Name required";
+                        }
+                        break;
+                    case "LastName":
+                        if (string.IsNullOrEmpty(_lastName))
+                        {
+                            error = "Last Name required";
+                        }
+                        break;
+                    case "Age":
+                        if ((_age < 18) || (_age > 85))
+                        {
+                            error = "Age out of range.";
+                        }
+                        break;
+                }
+                return (error);
+            }
+        }
+
+        #endregion //IDataErrorInfo
+    }
+}
---
+++ SimpleViewModel/ViewModels/MainViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace SimpleMvvm
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+
+        private Person _modelPerson;
+        public Person ModelPerson
+        {
+            get { return _modelPerson; }
+            set
+            {
+                _modelPerson = value;
+                OnPropertyChanged("ModelPerson");
+            }
+        }
+
+        private SavePersonCommand _savePersonCommand;
+        public SavePersonCommand SavePersonCommand
+        {
+            get { return _savePersonCommand; }
+            set
+            {
+                _savePersonCommand = value;
+                OnPropertyChanged("SavePersonCommand");
+            }
+        }
+
+        #endregion //Properties
+
+        #region Constructors
+
+        public MainViewModel()
+        {
+            InitializeCommands();
+            LoadPerson();
+        }
+
+        #endregion //Constructors
+
+        #region Methods
+
+        private void LoadPerson()
+        {
+            ModelPerson = new Person()
+            {
+                FirstName = "Brian",
+                LastName = "Lagunas",
+                Age = 30
+            };
+        }
+
+        private void InitializeCommands()
+        {
+            SavePersonCommand = new SavePersonCommand(UpdatePerson);
+        }
+
+        private void UpdatePerson()
+        {
+            ModelPerson.LastUpdated = DateTime.Now;
+        }
+
+        #endregion //Methods
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyname)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyname));
+        }
+
+        #endregion //INotifyPropertyChanged
+    }
+
+    public class SavePersonCommand : ICommand
+    {
+        Action _executeMethod;
+
+        public SavePersonCommand(Action executeAction)
+        {
+            _executeMethod = executeAction;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _executeMethod.Invoke();
+        }
+    }
+}
